e56953ec00d56bdffa08e61822a8aec1034fdfff
Artem Kajalainen <artem.kajalainen@gofore.com>
2018-01-26 16:00:54 +0200

    Add partials and footer

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 
+hbs.registerPartials(`${__dirname}/views/partials`);
 app.set('view engine', 'hbs');
 app.use(express.static(`${__dirname}/public`));
 
@@ -26,6 +27,6 @@ app.get('/bad', (req, res) => {
     });
 })
 
-app.listen(12947, () => {
+app.listen(3000, () => {
     console.log('Server is up on port 12947');
 });
